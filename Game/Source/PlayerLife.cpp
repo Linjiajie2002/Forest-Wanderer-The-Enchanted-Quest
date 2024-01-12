0db5a6d64930fa4b9285ff8a3dc5cb6f8b596cf6
Linjiajie2002 <92087408+Linjiajie2002@users.noreply.github.com>
2024-01-12 14:06:17 +0100

    JJL-LifeBar

--- Game/Source/PlayerLife.cpp
+++ Game/Source/PlayerLife.cpp
@@ -25,23 +25,54 @@ PlayerLife::~PlayerLife() {}
 
 bool PlayerLife::Awake() {
 
-	lifebar_fin_die_texture_Path = parameters.child("lifebar_fin_die").attribute("texturepath").as_string();
-	TSprite = parameters.child("lifebar_fin_die").attribute("Tsprite").as_int();
-	SpriteX = parameters.child("lifebar_fin_die").attribute("x").as_int();
-	SpriteY = parameters.child("lifebar_fin_die").attribute("y").as_int();
-	PhotoWeight = parameters.child("lifebar_fin_die").attribute("Pweight").as_int();
+	lifebar_head_tail_texture_Path = parameters.child("lifebar_head_tail").attribute("texturepath").as_string();
+	TSprite = parameters.child("lifebar_head_tail").attribute("Tsprite").as_int();
+	SpriteX = parameters.child("lifebar_head_tail").attribute("x").as_int();
+	SpriteY = parameters.child("lifebar_head_tail").attribute("y").as_int();
+	PhotoWeight = parameters.child("lifebar_head_tail").attribute("Pweight").as_int();
 	spritePositions = SPosition.SpritesPos(TSprite, SpriteX, SpriteY, PhotoWeight);
-	fin_die.LoadAnim("PlayerLife", "lifebar_fin_die", spritePositions);
+	//Head
+	Head_idle.LoadAnim("PlayerLife_head", "Head_idle", spritePositions);
+	Head_treatment.LoadAnim("PlayerLife_head", "Head_treatment", spritePositions);
+	Head_takehit.LoadAnim("PlayerLife_head", "Head_takehit", spritePositions);
+	Head_die.LoadAnim("PlayerLife_head", "Head_die", spritePositions);
+	//Tail
+	Tail_treatment_nb.LoadAnim("PlayerLife_head", "Tail_treatment_nb", spritePositions);
+	Tail_treatment_ab.LoadAnim("PlayerLife_head", "Tail_treatment_ab", spritePositions);
+	Tail_takehit_nb.LoadAnim("PlayerLife_head", "Tail_takehit_nb", spritePositions);
+	Tail_takehit_ab.LoadAnim("PlayerLife_head", "Tail_takehit_ab", spritePositions);
+	Tail_die.LoadAnim("PlayerLife_head", "Tail_die", spritePositions);
+	Tail_idle.LoadAnim("PlayerLife_head", "Tail_idle", spritePositions);
+
+
+	lifebar_mid_texture_Path = parameters.child("lifebar_mid").attribute("texturepath").as_string();
+	TSprite = parameters.child("lifebar_mid").attribute("Tsprite").as_int();
+	SpriteX = parameters.child("lifebar_mid").attribute("x").as_int();
+	SpriteY = parameters.child("lifebar_mid").attribute("y").as_int();
+	PhotoWeight = parameters.child("lifebar_mid").attribute("Pweight").as_int();
+	spritePositions = SPosition.SpritesPos(TSprite, SpriteX, SpriteY, PhotoWeight);
+	
+	Middle_idle.LoadAnim("PlayerLife_mid", "Middle_idle", spritePositions);
+	Middle_treatment_wb.LoadAnim("PlayerLife_mid", "Middle_treatment_wb", spritePositions);
+	Middle_treatment_nb.LoadAnim("PlayerLife_mid", "Middle_treatment_nb", spritePositions);
+	Middle_treatment_ab.LoadAnim("PlayerLife_mid", "Middle_treatment_ab", spritePositions);
+	Middle_takehit_wb.LoadAnim("PlayerLife_mid", "Middle_takehit_wb", spritePositions);
+	Middle_takehit_nb.LoadAnim("PlayerLife_mid", "Middle_takehit_nb", spritePositions);
+	Middle_takehit_ab.LoadAnim("PlayerLife_mid", "Middle_takehit_ab", spritePositions);
+	Middle_die.LoadAnim("PlayerLife_mid", "Middle_die", spritePositions);
 
 	return true;
 }
 
 bool PlayerLife::Start() {
-	lifebar_fin_die_texture = app->tex->Load(lifebar_fin_die_texture_Path);
+	lifebar_head_tail_texture = app->tex->Load(lifebar_head_tail_texture_Path);
+
+	lifebar_mid_texture = app->tex->Load(lifebar_mid_texture_Path);
 
 
-	currentAnimation1 = &fin_die;
+	currentAnimation1 = &Head_idle;
 
+	currentAnimation2 = &Middle_idle;
 
 
 	return true;
@@ -49,12 +80,24 @@ bool PlayerLife::Start() {
 
 bool PlayerLife::Update(float dt)
 {
+	//36
 
-
+	currentAnimation1 = &Head_die;
 	rect_1 = currentAnimation1->GetCurrentFrame();
-	app->render->DrawTexture(lifebar_fin_die_texture, 1920, 950, 4, SDL_FLIP_NONE, &rect_1);
-
+	app->render->DrawTexture(lifebar_head_tail_texture, 60, 15, 2, SDL_FLIP_NONE, &rect_1, 0, 0);
 	currentAnimation1->Update();
+
+
+	currentAnimation2 = &Middle_die;
+	for (int i = 0; i < 1; i++)
+	{
+		lifePos_X = 96 + i * 30;
+		rect_2 = currentAnimation2->GetCurrentFrame();
+		app->render->DrawTexture(lifebar_mid_texture, lifePos_X, 15, 2, SDL_FLIP_NONE, &rect_2, 0, 0);
+		currentAnimation2->Update();
+	}
+
+	
 
 
 	return true;
--- Game/Source/PlayerLife.h
+++ Game/Source/PlayerLife.h
@@ -40,22 +40,29 @@ public:
 	bool isPicked = false;
 
 	Animation* currentAnimation1 = nullptr;
-	List<Animation*> currentAnimation2;
-	Animation* currentAnimation3 = nullptr;
-	Animation* currentAnimation4 = nullptr;
+	Animation* currentAnimation2 = nullptr;;
 
-	Animation fin_die;
-	
-	
-	List<Animation> atack_2;
-	Animation atack_3;
 
-	Animation ball_blue_start;
-	Animation ball_blue_running;
-	Animation ball_blue_end;
+	Animation Head_treatment;
+	Animation Head_takehit;
+	Animation Head_die;
+	Animation Head_idle;
 
-	Animation die;
-	Animation take_hit;
+	Animation Middle_treatment_wb;
+	Animation Middle_treatment_nb;
+	Animation Middle_treatment_ab;
+	Animation Middle_takehit_wb;
+	Animation Middle_takehit_nb;
+	Animation Middle_takehit_ab;
+	Animation Middle_die;
+	Animation Middle_idle;
+
+	Animation Tail_treatment_nb;
+	Animation Tail_treatment_ab;
+	Animation Tail_takehit_nb;
+	Animation Tail_takehit_ab;
+	Animation Tail_die;
+	Animation Tail_idle;
 
 
 
@@ -71,60 +78,22 @@ public:
 
 	bool isFacingLeft;
 
-	//Atack 1
-	bool getPlayerPosition = false;
-	int player_x;
-	Timer atack1_Getposition;
+	int lifePos_X;
 
-	//Atack 2
-	Timer atack2_generAtack;
-	Animation inicializaAnimation;
-	int numeroAtack = 4;
-	int maxNumAtack = 0;
-	bool allPrint = false;
-	int distancia = 0;
-
-	//Atack3
-	bool direction_Atack = false;
-
-	//Atack4
-	float velocitat = 0;
-
-	float atack4_posX;
-	int atack4_posX_R = 1420;
-
-	int atack4_posX_L = 2290;
-	int atack4_posY;
-
-
-	int deadenemy;
-
-	bool inBossBattle = false;
-	bool tpToinBossBattle = false;
-
-
-	double result_x = 0.00;
-	double result_y = 0.00;
 
 
 private:
 
-	SDL_Texture* lifebar_fin_die_texture;
-	SDL_Texture* boss_atack_2_texture;
-	SDL_Texture* boss_atack_3_texture;
-	SDL_Texture* boss_atack_4_texture;
+	SDL_Texture* lifebar_head_tail_texture;
+	SDL_Texture* lifebar_mid_texture;
 
-	const char* lifebar_fin_die_texture_Path;
-	const char* boss_atack_2_texture_Path;
-	const char* boss_atack_3_texture_Path;
-	const char* boss_atack_4_texture_Path;
 
+	const char* lifebar_head_tail_texture_Path;
+	const char* lifebar_mid_texture_Path;
 
 
 	SDL_Rect rect_1;
-	List<SDL_Rect> rect_2;
-	SDL_Rect rect_3;
-	SDL_Rect rect_4;
+	SDL_Rect rect_2;
 
 
 	SDL_Texture* Pathfindingtexture;
